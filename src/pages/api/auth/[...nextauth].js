30a1c18892f5074674e00bff6ef5ba4461c12e31
ROLAND KIRALY <43180177+toobig4u@users.noreply.github.com>
2021-06-16 21:26:18 +0200

    .

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,7 +5,7 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Google({
-      clientId: process.env.GOOGLE_ID,
+      clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
     // ...add more providers here
